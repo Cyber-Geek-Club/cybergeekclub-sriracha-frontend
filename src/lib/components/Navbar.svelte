cd1072e6a3dad08fd72d64358b891a72aa11ebff
PerthTheIamCat <thongcum2546@gmail.com>
2025-05-26 21:22:32 +0700

    feat: add Navbar component and update layout structure

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import { gsap } from 'gsap';
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		gsap.from('.nav-item', {
+			duration: 1,
+			opacity: 0,
+			y: -100,
+			stagger: 0.2
+		});
+		gsap.from('.nav-item1', {
+			duration: 1,
+			opacity: 0,
+			y: -100,
+			stagger: 0.2
+		});
+	});
+</script>
+
+<nav class="mx-5 flex items-center justify-between text-2xl">
+	<h1 class="nav-item">CyberGeekClub</h1>
+	<ul class="flex space-x-4">
+		<li class="nav-item1"><a href="/">Home</a></li>
+		<li class="nav-item1"><a href="/about">About</a></li>
+		<li class="nav-item1"><a href="/contact">Contact</a></li>
+	</ul>
+</nav>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,12 @@
 <script lang="ts">
 	import '../app.css';
+	import Navbar from '$lib/components/Navbar.svelte';
 
 	let { children } = $props();
 </script>
 
-{@render children()}
+<div class="grid min-h-screen grid-rows-[100px_1fr_100px]">
+	<Navbar />
+	{@render children()}
+	<div class="bg-green-500"></div>
+</div>
