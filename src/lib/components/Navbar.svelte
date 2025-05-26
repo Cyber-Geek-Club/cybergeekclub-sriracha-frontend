1117897292b19f70a72850c13ddf4b080b6733b5
PerthTheIamCat <thongcum2546@gmail.com>
2025-05-27 00:27:46 +0700

    feat: implement navbar animations and enhance layout structure

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,28 +1,32 @@
 <script lang="ts">
-	import { gsap } from 'gsap';
+	import { setupNavbarAnimation, addMouseFollowAnimation } from '$lib/animations/Navbar';
+	import { setupBasicHoverAnimation } from '../animations/basicHover';
 	import { onMount } from 'svelte';
+	import GradientBg from './gradientBG.svelte';
 
 	onMount(() => {
-		gsap.from('.nav-item', {
-			duration: 1,
-			opacity: 0,
-			y: -100,
-			stagger: 0.2
-		});
-		gsap.from('.nav-item1', {
-			duration: 1,
-			opacity: 0,
-			y: -100,
-			stagger: 0.2
-		});
+		setupNavbarAnimation().play();
+		setupBasicHoverAnimation().play();
+		addMouseFollowAnimation();
 	});
 </script>
 
-<nav class="mx-5 flex items-center justify-between text-2xl">
-	<h1 class="nav-item">CyberGeekClub</h1>
-	<ul class="flex space-x-4">
-		<li class="nav-item1"><a href="/">Home</a></li>
-		<li class="nav-item1"><a href="/about">About</a></li>
-		<li class="nav-item1"><a href="/contact">Contact</a></li>
-	</ul>
+<nav id="navbar" class="my-5 flex w-full justify-center">
+	<div class="flex h-[100px] w-4/5 items-center justify-around rounded-2xl border border-[#182D1F]">
+		<h1 class="gsap-hover logo follow-cursor follow-area cursor-default">CyberGeekClub</h1>
+		<div class="animate-bg flex rounded-xl border border-[#223027] bg-[#0E1B13] p-1">
+			<GradientBg>
+				<ul class="mx-4 flex space-x-4">
+					<li class="nav-item1 gsap-hover"><a href="/">Home</a></li>
+					<li class="nav-item2 gsap-hover"><a href="/about">About</a></li>
+					<li class="nav-item3 gsap-hover"><a href="/contact">Contact</a></li>
+				</ul>
+			</GradientBg>
+		</div>
+		<GradientBg>
+			<ul class="mx-4 flex">
+				<li class="nav-item4 gsap-hover"><a href="/register">Register</a></li>
+			</ul>
+		</GradientBg>
+	</div>
 </nav>
---
+++ src/lib/components/gradientBG.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	let { children } = $props();
+</script>
+
+<div class="animate-bg rounded-lg bg-radial from-[#0E1B13] from-80% to-transparent p-1">
+	<div class="flex rounded-md bg-gradient-to-t from-[#1C4A2C] to-[#182D1F] p-[2px]">
+		<div class="bg-primary rounded-sm p-2">
+			{@render children()}
+		</div>
+	</div>
+</div>
